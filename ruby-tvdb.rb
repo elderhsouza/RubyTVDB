e6f073e61ed46958460b640d98599ae816dc0d65
Elder Henrique Souza <esouza@2mundos.net>
2016-09-15 14:26:08 -0300

    refactored search method

--- ruby-tvdb.rb
+++ ruby-tvdb.rb
@@ -16,17 +16,16 @@ class RubyTVDB
   def search(query)
     request = HTTParty.get("#{@@mirror}/api/GetSeries.php?seriesname=#{query}")
 
-    results = Nokogiri::XML(request.body).xpath('//Series').map do |node|
+    Nokogiri::XML(request.body).xpath('//Series').map do |node|
       {
         id: node.search('seriesid').text,
         name: node.search('SeriesName').text
       }
     end
-    puts results
   end
 end
 
 test = RubyTVDB.new('D229EEECFE78BA5F')
 
 puts "RubyTVDB"
-test.search("House")
+puts test.search("Things")
